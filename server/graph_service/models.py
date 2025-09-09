cb014383472e2491e0b3ed974ce929a24f52db09
iztaylor <ztaylor@servicetitan.com>
2025-09-09 16:46:25 -0400

    Adds TeamMember custom entity and modifies Team custom entity.

--- server/graph_service/models.py
+++ server/graph_service/models.py
@@ -2,33 +2,38 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
+# Custom entity types
 class Team(BaseModel):
-    team: Optional[str] = Field("", description="The name of the team")
+    """A team that contains one or more team members"""
 
+    team_name: Optional[str] = Field(None, description='The name of the team'
+)
 
-# class Project(BaseModel):
-#     """A project"""
+class TeamMember(BaseModel):
+    """A team member, which is a person who is a member of one or more teams"""
 
-#     description: str | None = Field(default=None, description="General project info")
+    team_member_name: Optional[str] = Field(None, description='The name of the team member')
+    team_member_email: Optional[str] = Field(None, description='The email of the team member')
+    team_member_login: Optional[str] = Field(None, description='The login/username of the team member')
 
 
 class KubeResource(BaseModel):
     """A Kubernetes resource"""
 
-    namespace: Optional[str] = Field("", description="The namespace of the resource")
-    kind: Optional[str] = Field("", description="The kind of the resource")
-    component: Optional[str] = Field("", description="The component of the resource")
+    namespace: Optional[str] = Field('', description='The namespace of the resource')
+    kind: Optional[str] = Field('', description='The kind of the resource')
+    component: Optional[str] = Field('', description='The component of the resource')
 
 
 class ContainerImage(BaseModel):
     """A container image"""
 
-    image: Optional[str] = Field("", description="The name of the image")
+    image: Optional[str] = Field('', description='The name of the image')
     pipelineLink: Optional[str] = Field(
-        default=None, description="The TeamCity link of the pipeline"
+        default=None, description='The TeamCity link of the pipeline'
     )
     pipelineConfigId: Optional[str] = Field(
-        default=None, description="The TeamCity build config id of the pipeline"
+        default=None, description='The TeamCity build config id of the pipeline'
     )
 
 
@@ -39,19 +44,26 @@ class Cluster(BaseModel):
 class GitRepo(BaseModel):
     """A Git repository"""
 
-    #org: Optional[str] = Field("", description="The organization of the repository")
-    url: Optional[str] = Field("", description="The URL of the repository")
+    # org: Optional[str] = Field("", description="The organization of the repository")
+    url: Optional[str] = Field('', description='The URL of the repository')
 
 
+# class Project(BaseModel):
+#     """A project"""
+
+#     description: str | None = Field(default=None, description="General project info")
+
+
+# Custom edge types
 class IsMemberOf(BaseModel):
     """A relationship between a parent and a child"""
 
 
 class IsGitOpsRepoFile(BaseModel):
-    """A relationship between a parent and a child  """
+    """A relationship between a parent and a child"""
 
-    file_path: Optional[str] = Field("", description="The file path of the resource in the GitRepo")
-    github_link: Optional[str] = Field("", description="The GitHub link of the resource")
+    file_path: Optional[str] = Field('', description='The file path of the resource in the GitRepo')
+    github_link: Optional[str] = Field('', description='The GitHub link of the resource')
 
 
 class IsOwnedBy(BaseModel):
@@ -69,33 +81,35 @@ class IsDeployedTo(BaseModel):
 class IsInstanceOf(BaseModel):
     """A relationship between a parent and a child"""
 
-    instance: Optional[str] = Field("", description="The name of the instance")
-    component: Optional[str] = Field("", description="The component of the instance")
-    portal_link: Optional[str] = Field("", description="The HTTP link of the portal page")
+    instance: Optional[str] = Field('', description='The name of the instance')
+    component: Optional[str] = Field('', description='The component of the instance')
+    portal_link: Optional[str] = Field('', description='The HTTP link of the portal page')
 
 
 entity_types = {
     # "Project": Project,
-    "Team": Team,
-    "KubeResource": KubeResource,
-    "ContainerImage": ContainerImage,
-    "Cluster": Cluster,
-    "GitRepo": GitRepo,
+    'Team': Team,
+    'TeamMember': TeamMember,
+    'KubeResource': KubeResource,
+    'ContainerImage': ContainerImage,
+    'Cluster': Cluster,
+    'GitRepo': GitRepo,
 }
 edge_types = {
-    "IsMemberOf": IsMemberOf,
-    "IsOwnedBy": IsOwnedBy,
-    "IsGitOpsRepoFile": IsGitOpsRepoFile,
-    "IsCodeRepo": IsCodeRepo,
-    "IsDeployedTo": IsDeployedTo,
-    "IsInstanceOf": IsInstanceOf,
+    'IsMemberOf': IsMemberOf,
+    'IsOwnedBy': IsOwnedBy,
+    'IsGitOpsRepoFile': IsGitOpsRepoFile,
+    'IsCodeRepo': IsCodeRepo,
+    'IsDeployedTo': IsDeployedTo,
+    'IsInstanceOf': IsInstanceOf,
 }
 edge_type_map = {
     # ("Project", "Team"): ["IsOwnedBy"],
-    ("KubeResource", "Project"): ["IsInstanceOf"],
-    ("KubeResource", "Cluster"): ["IsDeployedTo"],
+    ('Team', 'TeamMember'): ['IsMemberOf'],
+    ('KubeResource', 'Project'): ['IsInstanceOf'],
+    ('KubeResource', 'Cluster'): ['IsDeployedTo'],
     # ("Project", "GitRepo"): ["IsGitOpsRepoFile", "IsCodeRepo"],
     # ("Project", "Cluster"): ["IsDeployedTo"],
-    ("ContainerImage", "KubeResource"): ["IsDeployedTo"],
-    ("Entity", "Entity"): ["RELATES_TO"],
+    ('ContainerImage', 'KubeResource'): ['IsDeployedTo'],
+    ('Entity', 'Entity'): ['RELATES_TO'],
 }
