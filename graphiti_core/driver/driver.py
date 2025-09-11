f93baffe811b6418e2e2779fc68f2e1205e45ced
Joseph Livecchi <jlivecchi@servicetitan.com>
2025-09-11 14:07:37 -0400

    Fixed runtime error

--- graphiti_core/driver/driver.py
+++ graphiti_core/driver/driver.py
@@ -163,7 +163,7 @@ class GraphDriver(ABC):
         ''  # Neo4j (default) syntax does not require a prefix for fulltext queries
     )
     _database: str
-    aoss_client: OpenSearch | None  # type: ignore
+    aoss_client: "OpenSearch | None"  # type: ignore
 
     @abstractmethod
     def execute_query(self, cypher_query_: str, **kwargs: Any) -> Coroutine:
